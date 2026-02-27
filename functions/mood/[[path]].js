f7c7664ae954bdb7b8a0b0e0044a51cbe42b2fd3
SJB7447 <sjb76337447@gmail.com>
2026-02-27 16:33:32 +0900

    Delete functions directory

--- functions/mood/[[path]].js
+++
@@ -1,7 +0,0 @@
-export async function onRequest(context) {
-    const url = new URL(context.request.url);
-    url.hostname = 'moodbrew.onrender.com';
-    // Render에서 호스트 충돌이 나지 않도록 새 Request 객체 생성
-    const request = new Request(url.toString(), context.request);
-    return fetch(request);
-}
