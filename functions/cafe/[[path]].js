8dbad8d93399b712210860895e48c9c0b4e8697d
SJB7447 <sjb76337447@gmail.com>
2026-02-27 15:11:05 +0900

    feat(cloudflare): recreate Pages Functions with new Request objects to fix 404s

--- functions/cafe/[[path]].js
+++ functions/cafe/[[path]].js
@@ -1,5 +1,7 @@
 export async function onRequest(context) {
     const url = new URL(context.request.url);
     url.hostname = 'mech-flow-main.vercel.app';
-    return fetch(url.toString(), context.request);
+    // Vercel에서 호스트 충돌이 나지 않도록 새 Request 객체 생성
+    const request = new Request(url.toString(), context.request);
+    return fetch(request);
 }
